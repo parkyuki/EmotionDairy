7e8c3e1a505422a59f24a18781a2dd31e3a5c336
Yuki Park <parkyk1223@naver.com>
2023-02-21 18:50:44 +0900

    f

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import CalenderEmotion from "./CalenderEmotion";
 
 const Calender = ({ curDate, today, diaryList }) => {
   const day = ["일", "월", "화", "수", "목", "금", "토"];
